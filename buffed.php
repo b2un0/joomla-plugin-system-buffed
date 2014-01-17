75590e8f72a9992c182dc0a256193f5b7ca29c1b
b2un0 <branko.wilhelm@gmail.com>
2014-01-17 19:01:26 +0100

    ide switch

--- buffed.php
+++ buffed.php
@@ -3,15 +3,17 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2013 Branko Wilhelm
+ * @copyright  (c) 2013 - 2014 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
- 
+
 defined('_JEXEC') or die;
 
-class plgSystemBuffed extends JPlugin {
+class plgSystemBuffed extends JPlugin
+{
 
-	public function onBeforeRender() {
-	    JFactory::getDocument()->addScript('http://wowdata.buffed.de/js/buffed-db-tooltips.js', 'text/javascript', false, true);
+    public function onBeforeRender()
+    {
+        JFactory::getDocument()->addScript('http://wowdata.buffed.de/js/buffed-db-tooltips.js', 'text/javascript', false, true);
     }
 }
